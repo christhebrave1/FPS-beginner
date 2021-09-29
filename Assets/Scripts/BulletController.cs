22c0fb3753a67a651bfa1a8911fa71fe932ff1a4
LAPTOP-502JSASH\chris <chrisjacobsen7@gmail.com>
2021-09-29 20:36:12 +0100

    added bullet life and destruction

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class BulletController : MonoBehaviour
 {
-    public float speed;
+    public float speed, bulletLife;
 
     public Rigidbody myRigidBody;
 
@@ -18,10 +18,22 @@ public class BulletController : MonoBehaviour
     void Update()
     {
         BulletFly();
+
+        bulletLife -= Time.deltaTime; // bulletLife = bulletLife - Time.deltaTime
+
+        if(bulletLife < 0)
+        {
+            Destroy(gameObject);
+        }
     }
 
     private void BulletFly()
     {
         myRigidBody.velocity = transform.forward * speed;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
     }
 }
