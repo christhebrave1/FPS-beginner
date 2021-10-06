194560981e8ba208562ad97009c5ca92cbf71dfd
LAPTOP-502JSASH\chris <chrisjacobsen7@gmail.com>
2021-10-06 19:26:28 +0100

    control over firing rate

--- Assets/Scripts/GunSystem.cs
+++ Assets/Scripts/GunSystem.cs
@@ -9,6 +9,9 @@ public class GunSystem : MonoBehaviour
     public Transform firePosition;
     public GameObject muzzleFlash, bulletHole, waterLeak;
     public GameObject bullet;
+    public bool canAutoFire;
+    private bool shooting, readyToShoot = true;
+    public float timeBetweenShots;
 
     // Start is called before the first frame update
     void Start()
@@ -24,12 +27,20 @@ public class GunSystem : MonoBehaviour
 
     private void Shoot()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (canAutoFire)
+            shooting = Input.GetMouseButton(0);
+        else
+            shooting = Input.GetMouseButtonDown(0);
+
+        if (shooting && readyToShoot)
         {
+            readyToShoot = false;
+
             RaycastHit hit;
 
             if (Physics.Raycast(myCameraHead.position, myCameraHead.forward, out hit, 100f))
             {
+
                 if (Vector3.Distance(myCameraHead.position, hit.point) > 2f)
                 {
 
@@ -62,7 +73,16 @@ public class GunSystem : MonoBehaviour
             Instantiate(muzzleFlash, firePosition.position, firePosition.rotation, firePosition);
 
             Instantiate(bullet, firePosition.position, firePosition.rotation, firePosition);
+
+            StartCoroutine(ResetShot());
         }
+    }
+
+    IEnumerator ResetShot()
+    {
+        yield return new WaitForSeconds(timeBetweenShots);
+
+        readyToShoot = true;
     }
 
 }
